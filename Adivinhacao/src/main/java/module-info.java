fe1531d2fc54d51e97347f2d57a3bfee89f1afe8
olu4P-12 <paulo81206@edu.unipar.br>
2025-11-16 16:05:22 -0300

    Delete Adivinhacao directory

--- Adivinhacao/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.adivinhacao {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.adivinhacao to javafx.fxml;
-    exports com.example.adivinhacao;
-}
